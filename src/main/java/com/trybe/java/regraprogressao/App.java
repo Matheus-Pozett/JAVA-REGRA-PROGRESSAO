8e10f3a4979a992ad878fc038fe2bf2361484423
Matheus Pozett <maa_souza11@hotmail.com>
2025-06-30 19:08:28 -0300

    Cria classe para cadastro do nome e peso, requisito 1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,22 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
-
   /**
    * Metodo main.
    */
   public static void main(String[] args) {
+    CadastroEstudante cadastroEstudante = new CadastroEstudante();
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    String qntAtividades = scanner.nextLine();
+
+    cadastroEstudante.cadastro(Integer.parseInt(qntAtividades));
+    scanner.close();
   }
 }
